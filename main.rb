5fec12dd8d1545b2ae9631f42b93ba07f0082fe6
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 18:08:16 +0200

    Vérification des réponses.

--- main.rb
+++ main.rb
@@ -20,19 +20,29 @@ class Gramclass
     choice = @file[n][0]
     puts choice
     answer = gets.chomp
-    if answer == @file[n][1]
-      puts "Bonne réponse"
-    else
-      puts "Mauvaise réponse, la bonne étais #{@file[n][1]}"
-    end
+    answer == @file[n][1] 
   end
 
 end
+
+
+round = 0.0
+r = 0.0
+
 
 linkwords = Gramclass.new "les Linking words", 'SRC/link_words.csv'
 linkwords.welcome
 
 while 1 != 0
-  linkwords.randomWord
+  result = linkwords.randomWord
+  round += 1
+  puts round
+  if result == true
+    r += 1
+    puts r
+    puts "#{((r/round)*100).to_i}% de bonnes réponses"
+  end
+
+
 end
 
