9ee4528be38e2ddf04eed3718b3fb17bd4451c25
BuildTools <unconfigured@null.spigotmc.org>
2020-09-12 14:42:04 +0200

    Vérification des réponses.

--- main.rb
+++ main.rb
@@ -2,9 +2,12 @@ require 'csv'
 
 class Gramclass
 
+  attr_accessor :file, :name
+
   def initialize(name, file)
     @name = name
-    @file = CSV.read file.to_s, col_sep: ';'
+    @file = CSV.read(file.to_s, col_sep: ';')
+    @title = @file.shift
     @size = @file.size
   end
 
@@ -13,11 +16,23 @@ class Gramclass
   end
 
   def randomWord
-    
+    n = rand(0..@size)
+    choice = @file[n][0]
+    puts choice
+    answer = gets.chomp
+    if answer == @file[n][1]
+      puts "Bonne réponse"
+    else
+      puts "Mauvaise réponse, la bonne étais #{@file[n][1]}"
+    end
   end
 
 end
 
 linkwords = Gramclass.new "les Linking words", 'SRC/link_words.csv'
 linkwords.welcome
-linkwords.randomWord
+
+while 1 != 0
+  linkwords.randomWord
+end
+
