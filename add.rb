aaf294f5a98473c03fc6b43b0564f816c9ddd6ee
Victor Morel <0victormorel@gmail.com>
2020-10-14 15:11:59 +0200

    -Wortschatz2 fini

---
+++ add.rb
@@ -0,0 +1,18 @@
+classes = `ls SRC`.split " "
+
+puts "Quel fichier voulez vous éditer?"
+
+
+for i in 1..classes.size
+  puts "#{i}- #{classes[i-1]}"
+end
+
+choice = gets.to_i - 1
+
+while 1!=0
+	puts "Francais:"
+	fr = gets.chomp
+	puts "Autre langue:"
+	trad = gets.chomp
+	puts "#{fr}:#{trad}"
+end
