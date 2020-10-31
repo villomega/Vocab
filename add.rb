078b4aae43e163bf897127bcae3481250ffb477a
Victor Morel <0victormorel@gmail.com>
2020-10-31 20:49:37 +0100

    - Ajout du système qui vérifie si l'entrée existe déjà ou non

--- add.rb
+++ add.rb
@@ -1,16 +1,43 @@
+require 'csv'
+system('clear')
+
 class Gram
 
-  attr_accessor :name, :file
+  attr_accessor :name, :file, :language
 
   def initialize(name, file)
     @name = name
     @file = file
+    @content = CSV.read(file.to_s, col_sep: ';')
+    @language = @content[0][1]
   end
 
   def welcome
     puts "Vous allez modifier #{@name}"
   end
 
+  def check french
+    n = 0
+    for i in 0...@content.size
+      if @content[i][0].to_s.include?(french)
+        puts "oui"
+        n += 1
+      end
+    end
+    if n > 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def newword french, other
+    if check(french) == true
+      puts "Contenu"
+    else
+      puts "Non!"
+    end
+  end
 end
 
 puts "Quel fichier voulez vous éditer?"
@@ -42,6 +69,13 @@ end
 gramclass = Gram.new "#{file.split(".")[0]}", "SRC/" + file
 gramclass.welcome
 
+while 1 != 0
+  puts "Francais"
+  fr = gets.chomp
+  puts gramclass.language
+  other = gets.chomp
+  gramclass.newword fr, other
+end
 
 
 
